fe921314bbf7f6bbe20b04e7581bce291794811e
Attila Dekany <dekany.attila@marktsoft.hu>
2023-02-05 11:33:43 +0100

    Add initial layout & category styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
+import './category.styles.scss';
 
 const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+    },
+    {
+      id: 4,
+      title: 'Womens',
+    },
+    {
+      id: 5,
+      title: 'Mens',
+    },
+  ]
+
   return (
-    <div>
-      Hello world
+    <div className="categories-container">
+
+      {categories.map(({ id, title }) => (
+
+        <div className="category-container" key={id}>
+          <div className="background-image" />
+          {/* <img /> */}
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Show Now</p>
+          </div>
+        </div>
+      ))}
+
+
+
+
     </div>
   );
 }
