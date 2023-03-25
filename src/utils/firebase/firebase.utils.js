58b796ea4ec4064641a211dc6955db7d139d316c
Attila Dekany <dekanyattila94@gmail.com>
2023-03-25 15:50:28 +0100

    rename to categories context & provider (instead of products)

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -9,7 +9,7 @@ import {
     signOut,
     onAuthStateChanged,
 } from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc, collection, writeBatch } from 'firebase/firestore';
+import { getFirestore, doc, getDoc, setDoc, collection, writeBatch, query, getDocs } from 'firebase/firestore';
 const ANONYMOUS_ICON_PATH =
     'https://resumegenius.com/wp-content/uploads/resume-profile-icon.png';
 
@@ -38,6 +38,7 @@ export const signInWithGoogleRedirect = () =>
 
 export const db = getFirestore();
 
+// one time initializer used to set categories collection
 export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
     const collectionRef = collection(db, collectionKey);
     const batch = writeBatch(db);
@@ -49,6 +50,21 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
 
     await batch.commit();
     console.log('done');
+}
+
+export const getCategoriesAndDocuments = async () => {
+    const collectionRef = collection(db, 'categories');
+    const q = query(collectionRef);
+
+    const querySnapshot = await getDocs(q);
+    const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+        const { title, items } = docSnapshot.data();
+        acc[title.toLowerCase()] = items;
+
+        return acc;
+    }, {});
+
+    return categoryMap;
 }
 
 export const createUserDocumentFromAuth = async (
