7106261d932dcf311a8e4bd7e74e6dd71b51e083
Attila Dekany <dekanyattila94@gmail.com>
2023-04-09 19:07:29 +0200

    refactor: change to styled component product-card comp.

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,28 +1,36 @@
 import { useContext } from 'react';
-import { CartContext } from '../../context/cart.context';
+
+import { CartContext } from '../../contexts/cart.context';
+
 import Button, { BUTTON_TYPES_CLASSES } from '../button/button.component';
-import './product-card.styles.scss';
+
+import {
+  ProductCartContainer,
+  Footer,
+  Name,
+  Price,
+} from './product-card.styles';
 
 const ProductCard = ({ product }) => {
-  const { id, name, imageUrl, price } = product;
+  const { name, price, imageUrl } = product;
   const { addItemToCart } = useContext(CartContext);
 
   const addProductToCart = () => addItemToCart(product);
 
   return (
-    <div className="product-card-container">
+    <ProductCartContainer>
       <img src={imageUrl} alt={`${name}`} />
-      <div className="footer">
-        <span className="name">{name}</span>
-        <span className="price">{price}</span>
-      </div>
+      <Footer>
+        <Name>{name}</Name>
+        <Price>{price}</Price>
+      </Footer>
       <Button
         buttonType={BUTTON_TYPES_CLASSES.inverted}
         onClick={addProductToCart}
       >
         Add to card
       </Button>
-    </div>
+    </ProductCartContainer>
   );
 };
 
