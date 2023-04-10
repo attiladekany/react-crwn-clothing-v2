ef520d88367136e28d25e69f8fee74d1085480e9
Attila Dekany <dekanyattila94@gmail.com>
2023-04-10 22:19:42 +0200

    customLoggerMiddleware

--- src/store/store.js
+++ src/store/store.js
@@ -2,8 +2,23 @@ import { legacy_createStore as createStore, compose, applyMiddleware } from 'red
 import logger from 'redux-logger'
 import { rootReducer } from './root-reducer'
 
-const middleWares = [logger];
+// const middleWares = [logger];
 
+const customLoggerMiddleware = (store) => (next) => (action) => {
+    if (!action.type) {
+        return next(action);
+    }
+    
+    console.log('type: ', action.type);
+    console.log('payload: ', action.payload);
+    console.log('currentState: ', store.getState());
+    
+    next(action);
+    
+    console.log('next state: ', store.getState());
+}
+
+const middleWares = [customLoggerMiddleware];
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
 // Todo rework legacy solution
