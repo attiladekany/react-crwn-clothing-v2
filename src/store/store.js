df838c565a78c4e037e6012c5bd54cf1c45b8577
Attila Dekany <dekanyattila94@gmail.com>
2023-04-13 19:52:08 +0200

    feat: add redux-persist lib - implement hidrate & de-hidrate state from/to localstorage

--- src/store/store.js
+++ src/store/store.js
@@ -1,20 +1,29 @@
 import { legacy_createStore as createStore, compose, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
 import { rootReducer } from './root-reducer'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 // const middleWares = [logger];
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const customLoggerMiddleware = (store) => (next) => (action) => {
     if (!action.type) {
         return next(action);
     }
-    
+
     console.log('type: ', action.type);
     console.log('payload: ', action.payload);
     console.log('currentState: ', store.getState());
-    
+
     next(action);
-    
+
     console.log('next state: ', store.getState());
 }
 
@@ -22,4 +31,6 @@ const middleWares = [customLoggerMiddleware];
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
 // Todo rework legacy solution
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(persistedReducer, undefined, composedEnhancers);
+
+export const persistor = persistStore(store);
