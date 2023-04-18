f4159aee9ba791f8407ec11644c63c1ff13322ad
Attila Dekany <dekanyattila94@gmail.com>
2023-04-18 19:07:48 +0200

    fix: payment form - add missing return

--- src/components/payment-form/payment.form.component.jsx
+++ src/components/payment-form/payment.form.component.jsx
@@ -32,7 +32,9 @@ const PaymentForm = () => {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({ amount: amount * 100 }),
-    }).then((res) => res.json());
+    }).then((res) => {
+      return res.json();
+    });
 
     const {
       paymentIntent: { client_secret },
