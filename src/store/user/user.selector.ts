fc081682b24c9c3692290c570ebfeb89735e47bf
Attila Dekany <dekanyattila94@gmail.com>
2023-04-25 19:59:47 +0200

    refactor: Typing - user redux

---
+++ src/store/user/user.selector.ts
@@ -0,0 +1,7 @@
+import { RootState } from '../store';
+import { UserState } from './user.reducer';
+import { createSelector } from 'reselect';
+
+export const selectUserReducer = (state: RootState): UserState => state.user;
+
+export const selectCurrentUser = createSelector(selectUserReducer, (user) => user.currentUser);
