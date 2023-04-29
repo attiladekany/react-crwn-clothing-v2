93c1e7a328a0dfe5e24ad31c50637728fad8c485
Attila Dekany <dekanyattila94@gmail.com>
2023-04-29 10:10:54 +0200

    refactor: clean useMemo poc

--- src/components/cart-dropdown/cart-dropdown.component.tsx
+++ src/components/cart-dropdown/cart-dropdown.component.tsx
@@ -4,30 +4,11 @@ import { useNavigate } from 'react-router-dom';
 import { CartDropdownContainer, CartItems, EmptyMessage } from './cart-dropdown.styles';
 import { useSelector } from 'react-redux';
 import { selectCartItems } from '../../store/cart/cart.selector';
-import { useCallback, useMemo, useState } from 'react';
-
-const sleep = (milliseconds: number): void => {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if (new Date().getTime() - start > milliseconds) {
-      break;
-    }
-  }
-};
+import { useCallback, useState } from 'react';
 
 const CartDropdown = () => {
   const cartItems = useSelector(selectCartItems);
   const navigate = useNavigate();
-
-  const [count, setCount] = useState(0);
-
-  const hundredCount = useMemo(() => {
-    console.log('start');
-    sleep(2 * 1000);
-    console.log('end');
-
-    return 100 + count;
-  }, [count]);
 
   // actual callback, that wants to be memooized, [dependency array determine when to rememoize]
   const goToCheckoutHandler = useCallback(() => {
@@ -37,14 +18,13 @@ const CartDropdown = () => {
   return (
     <CartDropdownContainer>
       <CartItems>
-        {hundredCount}
-        {/* {cartItems.length ? (
+        {cartItems.length ? (
           cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
         ) : (
           <EmptyMessage>Your cart is empty</EmptyMessage>
-        )} */}
+        )}
       </CartItems>
-      <Button onClick={() => setCount(count + 1)}>GO TO CHECKOUT</Button>
+      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
     </CartDropdownContainer>
   );
 };
