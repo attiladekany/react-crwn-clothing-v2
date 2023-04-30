2f63bb0655adcfcf5fffa63864ca20af59d3e6a7
Attila Dekany <x_attila.dekany@nagel-group.com>
2023-04-30 18:09:11 +0000

    fix: sign in/out layout for mobile

--- src/routes/authentication/authentication.component.tsx
+++ src/routes/authentication/authentication.component.tsx
@@ -5,8 +5,10 @@ import './authentication.styles.scss';
 const Authentication = () => {
   return (
     <div className="authentication-container">
-      <SignInForm />
-      <SignUpForm />
+      <div className="form-container">
+        <SignInForm />
+        <SignUpForm />
+      </div>
     </div>
   );
 };
